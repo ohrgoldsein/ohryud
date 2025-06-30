c7c8e5ce999dfa84f48dad305d46b1a5157dd3e3
ohrgoldsein <37366407+ohrgoldsein@users.noreply.github.com>
2025-06-30 18:06:00 +0300

    publication card style

--- src/components/PublicationCard.jsx
+++ src/components/PublicationCard.jsx
@@ -12,7 +12,7 @@ function PublicationCard({ image, title, articles = [], blogUrl }) {
   <img
     src={image}
     alt={title}
-    className="w-40 h-40 rounded-full object-cover border-4 border-[#333] shadow-md hover:brightness-110 transition duration-300"
+    className="w-40 h-40 rounded-full object-cover border-4 border-[#333]  filter brightness-75 contrast-125 sepia-[0.4] grayscale-[0.3] shadow-md hover:brightness-110 transition duration-300"
   />
 </div>
 
